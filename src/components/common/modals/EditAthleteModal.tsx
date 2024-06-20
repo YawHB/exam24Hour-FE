69d53d5595edca637fb881486af5b3d4ddd973d8
YawHB <yawhboateng@gmail.com>
2024-06-20 23:16:15 +0200

    created disciplines checkpoints ready to select :D

--- src/components/common/modals/EditAthleteModal.tsx
+++ src/components/common/modals/EditAthleteModal.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { IAthlete } from '../../../models/athlete';
+import React, { useEffect, useState } from 'react';
+import { IAthlete, IDiscipline } from '../../../models/athlete';
 import { Button, Dialog, Card, CardHeader, CardBody, CardFooter, Typography, Input, Checkbox } from '@material-tailwind/react';
+import { getAllDisciplines } from '../../../apiHandlers/get/apiGetAllDisciplines';
 
 interface DialogWithFormProps {
     open: boolean;
@@ -9,39 +10,85 @@ interface DialogWithFormProps {
 }
 
 export default function EditAthleteModal({ open, onClose, athlete }: DialogWithFormProps) {
+    const [selectedDisciplines, setSelectedDisciplines] = useState<string[]>([]);
+    const [allDisciplines, setAllDisciplines] = useState<IDiscipline[]>([]);
+
+    useEffect(() => {
+        getAllDisciplines().then((discipliner) => {
+            if (discipliner) {
+                setAllDisciplines(discipliner);
+            }
+        });
+    }, []);
+
+    useEffect(() => {
+        if (athlete && athlete.disciplines) {
+            setSelectedDisciplines(athlete.disciplines.map((discipline) => discipline.name));
+        }
+    }, [athlete]);
+
+    const handleDisciplinesChange = (disciplineName: string) => {
+        setSelectedDisciplines((prevSelectedDisciplines) => {
+            if (prevSelectedDisciplines.includes(disciplineName)) {
+                // Hvis disciplinen allerede er valgt, fjern den fra valgte discipliner
+                return prevSelectedDisciplines.filter((name) => name !== disciplineName);
+            } else {
+                // Hvis disciplinen ikke er valgt, tilføj den til valgte discipliner
+                return [...prevSelectedDisciplines, disciplineName];
+            }
+        });
+    };
+
     return (
         <>
-            <Dialog size="xs" open={open} handler={onClose} className="bg-transparent shadow-none">
+            <Dialog size="xs" open={open} handler={onClose} className="bg-transparent shadow-none h-[42rem] overflow-scroll">
                 <Card className="mx-auto w-full max-w-[24rem]">
                     <CardBody className="flex flex-col gap-4">
                         <Typography variant="h4" color="blue-gray">
-                            Sign In
+                            Rediger Atlet Information
                         </Typography>
                         <Typography className="mb-3 font-normal" variant="paragraph" color="gray">
-                            Enter your email and password to Sign In.
+                            ...
                         </Typography>
                         <Typography className="-mb-2" variant="h6">
-                            Your Email
+                            Navn
                         </Typography>
-                        <Input label="Email" size="lg" />
+                        <Input label={athlete?.name} size="lg" disabled crossOrigin="" />
                         <Typography className="-mb-2" variant="h6">
-                            Your Password
+                            Køn
                         </Typography>
-                        <Input label="Password" size="lg" />
-                        <div className="-ml-2.5 -mt-3">
-                            <Checkbox label="Remember Me" />
-                        </div>
+                        <Input label={athlete?.sex} size="lg" disabled crossOrigin="" />
+                        <Typography className="-mb-2" variant="h6">
+                            Alder
+                        </Typography>
+                        <Input label={athlete?.age?.toString()} size="lg" disabled crossOrigin="" />
+                        <Typography className="-mb-2" variant="h6">
+                            Klub
+                        </Typography>
+                        <Input label={athlete?.club} size="lg" disabled crossOrigin="" />
+                        <Typography className="-mb-2" variant="h6">
+                            Discipliner
+                        </Typography>
+                        {allDisciplines.map((discipline) => (
+                            <Checkbox
+                                crossOrigin=""
+                                key={discipline.id}
+                                color="green"
+                                checked={selectedDisciplines.includes(discipline.name)}
+                                onChange={() => handleDisciplinesChange(discipline.name)}
+                                label={discipline.name}
+                            ></Checkbox>
+                        ))}
                     </CardBody>
                     <CardFooter className="pt-0">
-                        <Button variant="gradient" onClick={onClose} fullWidth>
-                            Sign In
-                        </Button>
-                        <Typography variant="small" className="mt-4 flex justify-center">
-                            Don&apos;t have an account?
-                            <Typography as="a" href="#signup" variant="small" color="blue-gray" className="ml-1 font-bold" onClick={onClose}>
-                                Sign up
-                            </Typography>
-                        </Typography>
+                        <div className="flex">
+                            <Button variant="gradient" color="green" onClick={onClose} fullWidth>
+                                OK
+                            </Button>
+                            <Button variant="gradient" color="red" onClick={onClose} fullWidth>
+                                Fortryd
+                            </Button>
+                        </div>
                     </CardFooter>
                 </Card>
             </Dialog>
